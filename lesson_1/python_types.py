8849e5970727d50602aa736fb11e9fd74791ef0c
Ido Sekely <sekely@gmail.com>
2016-01-28 00:14:37 +0200

    first lesson, with code examples

--- lesson_1/python_types.py
+++ lesson_1/python_types.py
@@ -9,15 +9,15 @@ is considered
 as a string, or block
 '''
 
-1 + 1 # these are pure integers, complete numbers. the result is '2'
+1 + 1  # these are pure integers, complete numbers. the result is '2'
 
-3 / 1 # division ot integers will produce an integer. in that case, '3'.
+3 / 1  # division ot integers will produce an integer. in that case, '3'.
 
-5 / 3 # what is the result in that case?
+5 / 3  # what is the result in that case?
 
-1.0 + 1.0 # these are float numbers. they represent numbers with floating point. the result is '2.0'
+1.0 + 1.0  # these are float numbers. they represent numbers with floating point. the result is '2.0'
 
-5.0 / 3.0 # what about now?
+5.0 / 3.0  # what about now?
 
 '''
 so the result of two integers is integer.
@@ -33,6 +33,34 @@ try it!
 # is there any difference? (no!)
 # calling to "print" will always reproduce a string output!
 
-1 == 5 # this is boolean. it has only two results - True or False.
+1 == 5  # this is boolean. it has only two results - True or False.
 
-1 != 5 # now it's True!
+1 != 5  # now it's True!
+
+
+# Summary
+# math and operands
+5 / 3      # 1
+5.0 / 3    # 1.6666666666666667
+5.0 // 3   # 1.0
+5 % 3      # 2
+5 ** 3     # 125
+
+# strings and some string manipulation
+len('hello')                          # 5
+print 'hello ' + 'world'              # 'hello world'
+print 'this is %s format' % 'string'  # 'this is string format'
+print 'My Name'.upper()               # 'MY NAME'
+print 'My Name'.lower()               # 'my name'
+
+# boolean expression and algebra
+not False       # True
+not True        # False
+False and True  # False
+True and True   # True
+True or False   # True
+False or False  # False
+5 == 5          # True
+3 != 5          # True
+5 < 3           # False
+5 <= 5          # True
