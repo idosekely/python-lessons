d5f3ddeb802b65273b5ab75148fffdbbfa21c4ec
Ido Sekely <sekely@gmail.com>
2016-01-29 09:49:44 +0200

    lessons 1+2 - adding exercises

---
+++ lesson_1/exercise/exercise_1.py
@@ -0,0 +1,12 @@
+__author__ = 'sekely'
+from getpass import getpass
+
+'''
+1)
+write a simple code that will accept a user name and password.
+since raw_input doesn't hide the input, use getpass instead (already imported it for you)
+
+2)
+write simple sum calculator.
+should accept 2 numbers from the user, and print the sum
+'''
