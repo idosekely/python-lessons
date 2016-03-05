e56178f18e2dcc02eea7e7a07e761bebef6a5bb5
Ido Sekely <sekely@gmail.com>
2016-03-05 13:02:29 +0200

    lesson 5 - client server

---
+++ lesson_5/exercise/exercise_5.py
@@ -0,0 +1,14 @@
+__author__ = 'sekely'
+
+'''
+1)
+use the attached client-server files to send names to the server.
+the server should save those into file
+'''
+
+'''
+2) advanced
+build a simple server (using the attached files) and integrate it with the address book that you wrote.
+the server should listen to a protocol, and save the data to the address book.
+
+'''
--- lesson_5/server.py
+++ lesson_5/server.py
@@ -2,38 +2,55 @@ import socket
 import sys
 __author__ = 'sekely'
 
-
 class SimpleServer(object):
+
     def __init__(self, addr='localhost', port=50000, buf=1024):
         self.buf = buf
-        server_address = (addr, port)
-        print >>sys.stderr, 'setting up server up on %s port %s' % server_address
+        self.server_address = (addr, port)
+        print 'setting up server up on %s port %s' % self.server_address
+        self._bind()
 
+    def _bind(self):
         # Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(server_address)
+        self.sock.bind(self.server_address)
 
-    def start(self):
+    def _listen(self):
         # Listen for incoming connections
         self.sock.listen(1)
 
-        while True:
-            print >>sys.stderr, 'waiting for a connection'
-            # Wait for a connection
-            connection, client_address = self.sock.accept()
+    def _accept(self):
+        print 'waiting for a connection'
+        # Wait for a connection
+        connection, client_address = self.sock.accept()
+        return connection, client_address
 
+    def start(self):
+        self._listen()
+        while True:
+            connection, client_address = self._accept()
             try:
                 # Receive the data in small chunks and retransmit it
                 while True:
                     data = connection.recv(self.buf)
-                    print >>sys.stderr, 'received "%s"' % data
+                    print 'received "%s"' % data
                     if data:
-                        print >>sys.stderr, 'sending data back to the client'
+                        print 'sending data back to the client'
                         connection.sendall(data)
                     else:
-                        print >>sys.stderr, 'no more data from', client_address
+                        print 'no more data from', client_address
                         break
-
             finally:
                 # Clean up the connection
                 connection.close()
+
+    def stop(self):
+        print "\nclosing server"
+        self.sock.close()
+
+if __name__ == '__main__':
+    server = SimpleServer()
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
