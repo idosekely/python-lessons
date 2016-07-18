d1ca0100c545b90da14f8aa7574d10b6b9bb7681
Ido Sekely <sekely@gmail.com>
2016-07-18 21:05:15 +0300

    fixing python 3 issues in lesson 1

--- lesson_1/python_types.py
+++ lesson_1/python_types.py
@@ -11,13 +11,13 @@ as a string, or block
 
 1 + 1  # these are pure integers, complete numbers. the result is '2'
 
-3 / 1  # division ot integers will produce an integer. in that case, '3'.
+3 / 1  # division ot integers will produce an integer. in that case, '3.0'.
 
 5 / 3  # what is the result in that case?
 
 1.0 + 1.0  # these are float numbers. they represent numbers with floating point. the result is '2.0'
 
-5.0 / 3.0  # what about now?
+5 // 3  # what about now?
 
 '''
 so the result of two integers is integer.
@@ -40,18 +40,19 @@ try it!
 
 # Summary
 # math and operands
-5 / 3      # 1
+5 / 3      # 1.6666666666666667
 5.0 / 3    # 1.6666666666666667
 5.0 // 3   # 1.0
+5 // 3     # 1
 5 % 3      # 2
 5 ** 3     # 125
 
 # strings and some string manipulation
-len('hello')                          # 5
-print(('hello ' + 'world'))              # 'hello world'
-print(('this is %s format' % 'string'))  # 'this is string format'
-print(('My Name'.upper()))               # 'MY NAME'
-print(('My Name'.lower()))               # 'my name'
+len('hello')                             # 5
+print('hello ' + 'world')              # 'hello world'
+print('this is %s format' % 'string')  # 'this is string format'
+print('My Name'.upper())               # 'MY NAME'
+print('My Name'.lower())               # 'my name'
 
 # boolean expression and algebra
 not False       # True
