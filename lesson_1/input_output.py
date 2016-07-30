c822ae2da560370ede5dc17af29edda125ca3982
Ido Sekely <sekely@gmail.com>
2016-07-30 18:38:49 +0300

    solution for lesson 1

--- lesson_1/input_output.py
+++ lesson_1/input_output.py
@@ -2,17 +2,17 @@ __author__ = 'sekely'
 
 # you can insert your own data!
 x = input("please enter your name: ")
-print(('welcome to the first lesson, %s!' % x))
+print('welcome to the first lesson, %s!' % x)
 
 a = input('enter the first number: ')
 b = input('enter the second number: ')
 
 # what is the problem here?
-print(("the sum of these numbers is: %s" % (a + b)))
-print(("and the average is: %s" % ((a + b) / 2.0)))
+print("the sum of these numbers is: %s" % (a + b))
+print("and the average is: %s" % ((a + b) / 2.0))
 
 # let's fix!
-print(("the sum of these numbers is: %s" % (int(a) + int(b))))
-print(("and the average is: %s" % ((int(a) + int(b)) / 2.0)))
+print("the sum of these numbers is: %s" % (int(a) + int(b)))
+print("and the average is: %s" % ((int(a) + int(b)) / 2.0))
 
 # can we do it a bit better?
